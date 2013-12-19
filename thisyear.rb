bfee4a74f974fc35e3bb754c1f4d0b1082f2042c
tnkjapan <yuto.yasunaga@gmail.com>
2013-12-19 15:20:31 +0700

    英語から日本語にほんやくしました。

--- thisyear.rb
+++ thisyear.rb
@@ -1,11 +1,11 @@
 now = Time.now
 the_last_day = Time.mktime(2013, 12, 31, 23, 59, 59)
 hour_of_day = 24
-min_sec_of_hour = 60;
+min_sec_of_hour = 60
 
-puts "Now is #{now.year}/#{now.day}/#{now.month} - #{now.hour}:#{now.min}:#{now.sec}"
-
+puts "今は　#{now.year}年　#{now.month}月　#{now.day}日　#{now.hour}時　#{now.min}分　#{now.sec}秒　です。"
 month = the_last_day.month - now.month
+
 day = 0
 for i in now.month+1..12
     case i
