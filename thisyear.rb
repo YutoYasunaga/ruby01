d415714b4abd0ddb72421a16c63da009eb8a25ea
tnkjapan <yuto.yasunaga@gmail.com>
2013-12-19 15:16:24 +0700

    新しいのをプログラミングしました。

--- thisyear.rb
+++ thisyear.rb
@@ -1,22 +1,30 @@
+now = Time.now
+the_last_day = Time.mktime(2013, 12, 31, 23, 59, 59)
+hour_of_day = 24
+min_sec_of_hour = 60;
 
-time1 = Time.now
-time2 = Time.mktime(2013, 12, 31, 23, 59, 59)
+puts "Now is #{now.year}/#{now.day}/#{now.month} - #{now.hour}:#{now.min}:#{now.sec}"
 
-nichi = 31;
-jikan = 24;
-fun_byou = 60;
-month=time2.month-time1.month
-day=month*nichi+(nichi-time1.day)
-hour=day*jikan+(jikan-time1.hour)
-minute=hour*fun_byou+(fun_byou-time1.min)
-second=minute*fun_byou+(fun_byou-time1.sec)
+month = the_last_day.month - now.month
+day = 0
+for i in now.month+1..12
+    case i
+    when 1, 3, 5, 7, 8, 10, 12
+        day = day + 31
+    when 4, 6, 9, 11
+        day = day + 30
+    when 2
+        day = day + 28
+    end
+end
+day = day + (the_last_day.day - now.day)
 
-puts "今は　#{time1.year}年　#{time1.month}月　#{time1.day}日　｜　#{time1.hour}時　#{time1.min}分　#{time1.sec}秒"
+hour = day * hour_of_day + (hour_of_day - now.hour)
+minute = hour + min_sec_of_hour + (min_sec_of_hour - now.min)
+second = minute + min_sec_of_hour + (min_sec_of_hour - now.sec)
 
-puts "-----------"
-puts "今年は後："
-puts "#{month}月"
-puts "#{day}日"
-puts "#{hour}時"
-puts "#{minute}分"
-puts "#{second}秒"
+puts "今年は後　#{month}月　です。"
+puts "今年は後　#{day}日　です。"
+puts "今年は後　#{hour}時　です。"
+puts "今年は後　#{minute}分　です。"
+puts "今年は後　#{second}秒　です。"
