7c44990f278e2a79f0b717dd645c0dbf56805d54
tnkjapan <yuto.yasunaga@gmail.com>
2013-12-19 14:40:32 +0700

    Created magic numbers

--- thisyear.rb
+++ thisyear.rb
@@ -2,11 +2,14 @@
 time1 = Time.now
 time2 = Time.mktime(2013, 12, 31, 23, 59, 59)
 
+nichi = 31;
+jikan = 24;
+fun_byou = 60;
 month=time2.month-time1.month
-day=month*30+(31-time1.day)
-hour=day*24+(24-time1.hour)
-minute=hour*60+(60-time1.min)
-second=minute*60+(60-time1.sec)
+day=month*nichi+(nichi-time1.day)
+hour=day*jikan+(jikan-time1.hour)
+minute=hour*fun_byou+(fun_byou-time1.min)
+second=minute*fun_byou+(fun_byou-time1.sec)
 
 puts "今は　#{time1.year}年　#{time1.month}月　#{time1.day}日　｜　#{time1.hour}時　#{time1.min}分　#{time1.sec}秒"
 
